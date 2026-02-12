26d58a4f45d71e13c1fba629c0f8419f476108a9
Aravind <aravindganipisetty@gmail.com>
2026-02-11 20:00:34 -0500

    Initial Commit

---
+++ Task_Flow/Task_FlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_FlowApp.swift
+//  Task_Flow
+//
+//  Created by Aravind Ganipisetty on 2/11/26.
+//
+
+import SwiftUI
+
+@main
+struct Task_FlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Task_FlowTests/Task_FlowTests.swift
@@ -0,0 +1,17 @@
+//
+//  Task_FlowTests.swift
+//  Task_FlowTests
+//
+//  Created by Aravind Ganipisetty on 2/11/26.
+//
+
+import Testing
+@testable import Task_Flow
+
+struct Task_FlowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
